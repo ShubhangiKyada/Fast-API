b0e6dd0e3cc5774319d29c36fea031c2b48f39c3
shubhangi <shubhukyada@gmail.com>
2023-12-20 14:26:22 +0530

    [Shubhangi]first Api

---
+++ app.py
@@ -0,0 +1,22 @@
+from fastapi import FastAPI
+from typing import Union,Optional
+from pydantic import BaseModel
+
+app=FastAPI()
+
+class item(BaseModel):
+    item_id:int
+    q:Optional[str]=None
+
+@app.get("/")
+def read_root():
+    return {"Hello" : "world"}
+
+@app.get("/item/{item_id}")
+def read_item(item_id:int,q:Union[str,None]=None):
+    return {"item_id":item_id,"q":q}
+
+@app.post("/item/{item_id}")
+def create_item(item_id:int,q:Union[str,None]=None):
+    return {"item_id":item_id,"q":q}
+
